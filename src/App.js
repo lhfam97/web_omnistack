e50de1904c195d477e8fdcff6972bdb27f2c390b
Luis Henrique Machado <lhfam97@gmail.com>
2020-01-18 21:29:47 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import "./global.css";
+import "./app.css";
+import "./Sidebar.css";
+import "./main.css";
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo componentne (Lembrar: imutabilidade)
 
+// navigator.geolocation.getCurrentPosition; Localizador
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  function incrementCounter() {
-    setCounter(counter + 1);
+  const [devs, setDevs] = useState([]);
+  // const [github_username, setGithubUsername] = useState("");
+  // const [techs, setTechs] = useState("");
+  // const [latitude, setLatitude] = useState(0);
+  // const [longitude, setLongitude] = useState(0);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+    setGithubUsername("");
+    setTechs("");
+    setDevs([...devs, response.data]);
+    console.log(response.data);
   }
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>{" "}
-    </>
+    <div id="app">
+      <aside>
+        <strong className="">Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
+    </div>
   );
 }
 
